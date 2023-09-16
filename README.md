a43c17c241c76d82ace18d9234aebc350d4c7190
Jeriko Carrera <joshjbarba@gmail.com>
2023-09-15 21:48:45 -0700

    Created an array called pokemonList on scripts.js

---
+++ README.md
@@ -0,0 +1,4 @@
+Project description
+How to get the project running
+Project dependencies (e.g., JavaScript version, ESLint rules)
+Which API the project uses
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
-alert('Hello World');
+let pokemonList = [
+    
+    {name: "bulbasaur", weight: 15, type: ["grass","poison"]},
+    {name: "charmander", weight: 10, type: "fire"},
+    {name: "squirtle", weight: 8, type: "water"},
+    {name: "pikachu", weight: 5, type: "electric"},
+    {name: "eevee", weight: 7, type: "normal"},
+    {name: "nidoking", weight: 50, type: ["poison","ground"]},
+    {name: "nidoqueen", weight: 45, type: ["poison","ground"]}
 
-let favoritePokemon = "Nidoking";
-document.write (favoritePokemon);
+];
