4b65452183ebd657fc14873cbe7a57a23bcfed59
Jeriko Carrera <joshjbarba@gmail.com>
2023-09-15 23:43:11 -0700

    Added in some basic CSS and general idea for Pokedex App
    The idea is that once a pokemon's name is typed in, it will spawn that pokemon's sprite (ideally a 3d or at least gen 5 one). in the middle of the page while it's information is placed on the right.
    
    Also plan on adding arrows on both sides of the avatar to cycle between each pokemon by pokedex #.
    
    A list of all pokemon will be available as well perhaps on another page or maybe on the bottom before the footer.
    
    Also plan on adding a random button that brings you to a random pokemon.
    
    Not sure how any of these will work so far but I'm hoping to learn them in the upcoming chapters!

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,26 @@
 let pokemonList = [
     
-    {name: "bulbasaur", weight: 15, type: ["grass","poison"]},
-    {name: "charmander", weight: 10, type: "fire"},
-    {name: "squirtle", weight: 8, type: "water"},
-    {name: "pikachu", weight: 5, type: "electric"},
-    {name: "eevee", weight: 7, type: "normal"},
-    {name: "nidoking", weight: 50, type: ["poison","ground"]},
-    {name: "nidoqueen", weight: 45, type: ["poison","ground"]}
+    {name: "bulbasaur", pokedexNumber: "#001", weight: 15, type: ["grass","poison"]},
+    {name: "charmander",  pokedexNumber: "#004", weight: 10, type: "fire"},
+    {name: "squirtle", pokedexNumber: "#007", weight: 8, type: "water"},
+    {name: "pikachu", pokedexNumber: "#025", weight: 5, type: "electric"},
+    {name: "eevee", pokedexNumber: "#133", weight: 7, type: "normal"},
+    {name: "nidoking", pokedexNumber: "#034", weight: 50, type: ["poison","ground"]},
+    {name: "nidoqueen", pokedexNumber: "#031", weight: 45, type: ["poison","ground"]}
 
 ];
+
+// when the user click on the button, the following code is executed
+document.querySelector('button').addEventListener('click', () => {
+
+    let currentPokemon = document.querySelector('#Pokemon').value;
+
+    document.querySelector('#pokemon-name').innerText = currentPokemon;
+
+    //Commenting this section out because I'm not sure how to call these properties from object pokemonList yet.
+    /**document.querySelector('#pokedex-number').innerText =;
+    document.querySelector('#pokemon-weight').innerText =;
+    document.querySelector('#pokemon-type').innerText =;**/
+
+    
+  });
