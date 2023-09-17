7dbe5225066f194f1c05106cf64295bbecb9ba2b
Jeriko Carrera <joshjbarba@gmail.com>
2023-09-17 04:11:09 -0700

    added in Exercise 1.5, added in pokemonList2, a prototype TypeChart, did some formatting on the buttons as well.

--- js/scripts.js
+++ js/scripts.js
@@ -11,36 +11,37 @@ let pokemonList = [
 
 ];
 
-//variables to measure lightness and heaviness
-const lightPokemon = 15;
-const heavyPokemon = 30;
+let pokemonList2 = [
+    
+    {name: 'chikorita', pokedexNumber: '#152', weight: 14, type: 'Grass'},
+    {name: 'cindaquil',  pokedexNumber: '#155', weight: 17, type: 'Fire'},
+    {name: 'totadile', pokedexNumber: '#158', weight: 21, type: 'Water'},
+    {name: 'pichu', pokedexNumber: '#172', weight: 4.4, type: 'Electric'},
+    {name: 'umbreon', pokedexNumber: '#197', weight: 60, type: 'Dark'},
+    {name: 'espeon', pokedexNumber: '#196', weight: 58, type: 'Psychic'},
+    {name: 'steelix', pokedexNumber: '#208', weight: 885, type: ['Steel','Ground']}
 
-//empty variable used as a placeholder for writing everything down onto a list form on the HTML page
-var pokemon = '';
+];
 
-//for loop which checks for weight and gives values for each pokemons' properties
-for (let i = 0; i < pokemonList.length; i++){
-    if(pokemonList[i].weight >= heavyPokemon){
-        weightChecker = '(This pokemon is heavy!)';
-    }
-    else if (pokemonList[i].weight < heavyPokemon && pokemonList[i].weight > lightPokemon){
-        weightChecker = '(This pokemon is average)';
-    }
-    else{
-        weightChecker = '(This pokemon is light!)';
-    }
-
-    pokemon += pokemonList[i].name.charAt(0).toUpperCase()+ pokemonList[i].name.substring(1) + weightChecker + '<br>' ;
-    pokemon += pokemonList[i].pokedexNumber + '<br>';
-    pokemon += pokemonList[i].weight + ' lbs' + '<br>';
-    pokemon += pokemonList[i].type + '<br>' + '<br>' + '<br>';
-
-} 
-
-//This is what actually writes down the text
-document.querySelector('.pokemon-entry').innerHTML = pokemon;
-
-
+let TypeChart = [
+    {type: 'Normal', weaknesses: 'Fighting', resistances: 'N/A'},
+    {type: 'Grass', weaknesses: ['Fire','Flying','Ice','Bug','Poison'], resistances: 'N/A'},
+    {type: 'Fire', weaknesses: ['Water','Rock','Ground'], resistances: 'N/A'},
+    {type: 'Water', weaknesses: ['Electric','Grass'], resistances: 'N/A'},
+    {type: 'Electric', weaknesses: 'Ground', resistances: 'N/A'},
+    {type: 'Flying', weaknesses: ['Rock','Electric','Ice'], resistances: 'N/A'},
+    {type: 'Rock', weaknesses: ['Grass', 'Water', 'Ground', 'Steel', 'Fighting'], resistances: 'N/A'},
+    {type: 'Ground', weaknesses: ['Grass', 'Water', 'Ice'], resistances: 'N/A'},
+    {type: 'Ghost', weaknesses: ['Ghost', 'Dark'], resistances: 'N/A'},
+    {type: 'Bug', weaknesses: ['Fire', 'Flying', 'Rock',], resistances: 'N/A'},
+    {type: 'Psychic', weaknesses: ['Bug', 'Ghost', 'Dark'], resistances: 'N/A'},
+    {type: 'Dark', weaknesses: ['Fighting', 'Bug'], resistances: 'N/A'},
+    {type: 'Fairy', weaknesses: ['Poison', 'Steel'], resistances: 'N/A'},
+    {type: 'Dragon', weaknesses: ['Dragon', 'Fairy'], resistances: 'N/A'},
+    {type: 'Steel', weaknesses: ['Fire', 'Fighting', 'Ground'], resistances: 'N/A'},
+    {type: 'Ice', weaknesses: ['Fighting', 'Fire', 'Steel'], resistances: 'N/A'},
+    {type: 'Poison', weaknesses: ['Psychic', 'Ground'] ,resistances: 'N/A'}
+]
 
 //This code checks for when the user clicks the button then executes the following code when the user does.
 document.querySelector('button').addEventListener('click', () => {
@@ -50,6 +51,8 @@ document.querySelector('button').addEventListener('click', () => {
 
         //These are there to make it case insensitive
         let currentPokemon = PokemonInputted.toLowerCase();
+
+        //This makes sure the pokemon are capitalized correctly
         let properCase = currentPokemon.charAt(0).toUpperCase() + currentPokemon.substring(1);
 
 
@@ -57,15 +60,29 @@ document.querySelector('button').addEventListener('click', () => {
         document.querySelector('#pokemon-name').innerText = properCase;
         document.querySelector('#pokemon-Title').innerText = properCase;
 
-
-        //This writes down the pokemon's pokedex #, weight
-        for (let i = 0; i < pokemonList.length; i++){
-            if (pokemonList[i].name == currentPokemon) {
-                document.querySelector('#pokedex-number').innerText = pokemonList[i].pokedexNumber;
-                document.querySelector('#pokemon-weight').innerText = pokemonList[i].weight;
-                document.querySelector('#pokemon-type').innerText = pokemonList[i].type;
+        //This function writes down the pokemon's pokedex #, weight, and type
+        function pokemonStats(list){
+            for (let i = 0; i < list.length; i++){
+                if (list[i].name === currentPokemon) {
+                    document.querySelector('#pokedex-number').innerText = list[i].pokedexNumber;
+                    document.querySelector('#pokemon-weight').innerText = list[i].weight + ' lbs';
+                    document.querySelector('#pokemon-type').innerText = list[i].type;
+                    break;
+                }
+                // COMMENTING OUT DUE TO ME NOT KNOWING HOW TO PROPERLY INPUT THIS. 
+                // I need to understand how to implement these ONLY when list[i].name has no match with ANY of the currentPokemon.
+                // else if (list[i].name !== currentPokemon){
+                //     document.querySelector('#pokemon-Title').innerText = 'ERROR';
+                //     document.querySelector('#pokemon-name').innerText = 'ERROR. POKEMON IS NOT IN DATABASE.';
+                //     document.querySelector('#pokedex-number').innerText = 'ERROR. POKEMON IS NOT IN DATABASE.';
+                //     document.querySelector('#pokemon-weight').innerText = 'ERROR. POKEMON IS NOT IN DATABASE.';
+                //     document.querySelector('#pokemon-type').innerText = 'ERROR. POKEMON IS NOT IN DATABASE.';
+                // }
             }
         }
+
+        pokemonStats(pokemonList);
+        pokemonStats(pokemonList2);
 
         //This creates a picture of said pokemon. That said, it is just a placeholder for now and has no way of using the array
         const pokemonModel = document.createElement('img');
@@ -74,6 +91,45 @@ document.querySelector('button').addEventListener('click', () => {
         src.appendChild(pokemonModel);
     }
 )
+
+//variables to measure lightness and heaviness
+const lightPokemon = 20;
+const heavyPokemon = 80;
+const insanelyHeavyPokemon = 200;
+
+//empty variable used as a placeholder for writing everything down onto a list form on the HTML page
+var pokemon = '';
+
+//for loop which checks for weight and gives values for each pokemons' properties
+function pokedexEntry (list)
+{
+    for (let i = 0; i < list.length; i++){
+        if(list[i].weight >= insanelyHeavyPokemon){
+            weightChecker = '(Good luck carrying this pokemon!)';
+        }
+        else if(list[i].weight > heavyPokemon){
+            weightChecker = '(This pokemon is heavy!)';
+        }
+        else if (list[i].weight < heavyPokemon && list[i].weight > lightPokemon){
+            weightChecker = '(This pokemon is average)';
+        }
+        else{
+            weightChecker = '(This pokemon is light!)';
+        }
+
+        pokemon += list[i].name.charAt(0).toUpperCase()+ list[i].name.substring(1) + weightChecker + '<br>' ;
+        pokemon += list[i].pokedexNumber + '<br>';
+        pokemon += list[i].weight + ' lbs' + '<br>';
+        pokemon += list[i].type + '<br>' + '<br>' + '<br>';
+
+    } 
+
+//This is what actually writes down the text
+    document.querySelector('.pokemon-entry').innerHTML = pokemon;
+}
+
+pokedexEntry(pokemonList);
+pokedexEntry(pokemonList2);
 
 
 
