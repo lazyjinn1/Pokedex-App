f28fef93532424e26d2ed7852656f47da78ea0a6
Jeriko Carrera <joshjbarba@gmail.com>
2023-09-19 04:53:49 -0700

    Added JS to add in pictures of pokemon when searched for in the box. Also fixed and cleaned up many lines of code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,38 @@
+// var request = new XMLHttpRequest()
+
+// request.open('GET', 'http://pokeapi.co/api/v2/pokemon/?limit=1021',true)
+// request.onload = function (){
+//     var data = JSON.parse(this.response)
+// }
+
+// data.forEach ((pokemon) => {
+//     console.log(pokemon.name)
+// })
+
+// request.send()
+
 //array which shows all of our data.
 let pokemonList = [
     
-    {name: 'bulbasaur', pokedexNumber: '#001', weight: 15, type: ['Grass','Poison']},
-    {name: 'charmander',  pokedexNumber: '#004', weight: 19, type: 'Fire'},
-    {name: 'squirtle', pokedexNumber: '#007', weight: 19.8, type: 'Water'},
-    {name: 'pikachu', pokedexNumber: '#025', weight: 13, type: 'Electric'},
-    {name: 'eevee', pokedexNumber: '#133', weight: 14, type: 'Normal'},
-    {name: 'nidoking', pokedexNumber: '#034', weight: 137, type: ['Poison','Ground']},
-    {name: 'nidoqueen', pokedexNumber: '#031', weight: 132, type: ['Poison','Ground']}
+    {name: 'bulbasaur', pokedexNumber: '001', weight: 15, type: ['Grass','Poison']},
+    {name: 'charmander',  pokedexNumber: '004', weight: 19, type: 'Fire'},
+    {name: 'squirtle', pokedexNumber: '007', weight: 19.8, type: 'Water'},
+    {name: 'pikachu', pokedexNumber: '025', weight: 13, type: 'Electric'},
+    {name: 'eevee', pokedexNumber: '133', weight: 14, type: 'Normal'},
+    {name: 'nidoking', pokedexNumber: '034', weight: 137, type: ['Poison','Ground']},
+    {name: 'nidoqueen', pokedexNumber: '031', weight: 132, type: ['Poison','Ground']}
 
 ];
 
 let pokemonList2 = [
     
-    {name: 'chikorita', pokedexNumber: '#152', weight: 14, type: 'Grass'},
-    {name: 'cindaquil',  pokedexNumber: '#155', weight: 17, type: 'Fire'},
-    {name: 'totadile', pokedexNumber: '#158', weight: 21, type: 'Water'},
-    {name: 'pichu', pokedexNumber: '#172', weight: 4.4, type: 'Electric'},
-    {name: 'umbreon', pokedexNumber: '#197', weight: 60, type: 'Dark'},
-    {name: 'espeon', pokedexNumber: '#196', weight: 58, type: 'Psychic'},
-    {name: 'steelix', pokedexNumber: '#208', weight: 885, type: ['Steel','Ground']}
+    {name: 'chikorita', pokedexNumber: '152', weight: 14, type: 'Grass'},
+    {name: 'cyndaquil',  pokedexNumber: '155', weight: 17, type: 'Fire'},
+    {name: 'totodile', pokedexNumber: '158', weight: 21, type: 'Water'},
+    {name: 'pichu', pokedexNumber: '172', weight: 4.4, type: 'Electric'},
+    {name: 'umbreon', pokedexNumber: '197', weight: 60, type: 'Dark'},
+    {name: 'espeon', pokedexNumber: '196', weight: 58, type: 'Psychic'},
+    {name: 'steelix', pokedexNumber: '208', weight: 885, type: ['Steel','Ground']}
 
 ];
 
@@ -45,59 +58,57 @@ let pokemonList2 = [
 // ]
 
 //This code checks for when the user clicks the button then executes the following code when the user does.
-document.querySelector('button').addEventListener('click', () => {
+document.querySelector('button').addEventListener('click',()=>{
 
-        //This is the pokemon that is typed in the textbox
-        let PokemonInputted = document.querySelector('#Pokemon').value;
+    //function for adding in the pictures for pokemon
+    function createPokeImage(pokeID, containerDiv){
+        pokeImage = document.createElement('img');
+        pokeImage.srcset =`https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/${pokeID}.png`
+        var div = document.getElementById(containerDiv);
+        div.appendChild(pokeImage);
+    }
 
-        //These are there to make it case insensitive
-        let currentPokemon = PokemonInputted.toLowerCase();
+    //This is the pokemon that is typed in the textbox
+    let PokemonInputted = document.querySelector('#Pokemon').value;
 
-        //This makes sure the pokemon are capitalized correctly
-        let properCase = currentPokemon.charAt(0).toUpperCase() + currentPokemon.substring(1);
+    //These are there to make it case insensitive
+    let currentPokemon = PokemonInputted.toLowerCase();
 
+    //This makes sure the pokemon are capitalized correctly
+    let properCase = currentPokemon.charAt(0).toUpperCase() + currentPokemon.substring(1);
 
-        //This writes down the pokemon's name in the Title in the middle and on the side data
-        document.querySelector('#pokemon-name').innerText = properCase;
-        document.querySelector('#pokemon-Title').innerText = properCase;
-
-        //This function writes down the pokemon's pokedex #, weight, and type
-        function pokemonStats(list){
-            for (let i = 0; i < list.length; i++){
-                if (list[i].name === currentPokemon) {
-                    document.querySelector('#pokedex-number').innerText = list[i].pokedexNumber;
-                    document.querySelector('#pokemon-weight').innerText = list[i].weight + ' lbs';
-                    document.querySelector('#pokemon-type').innerText = list[i].type;
-                    break;
-                }
-                // COMMENTING OUT DUE TO ME NOT KNOWING HOW TO PROPERLY INPUT THIS. 
-                // I need to understand how to implement these ONLY when list[i].name has no match with ANY of the currentPokemon.
-                // else if (list[i].name !== currentPokemon){
-                //     document.querySelector('#pokemon-Title').innerText = 'ERROR';
-                //     document.querySelector('#pokemon-name').innerText = 'ERROR. POKEMON IS NOT IN DATABASE.';
-                //     document.querySelector('#pokedex-number').innerText = 'ERROR. POKEMON IS NOT IN DATABASE.';
-                //     document.querySelector('#pokemon-weight').innerText = 'ERROR. POKEMON IS NOT IN DATABASE.';
-                //     document.querySelector('#pokemon-type').innerText = 'ERROR. POKEMON IS NOT IN DATABASE.';
-                }
-            }
-
-        //This writes down the pokemon's pokedex #, weight
-        for (let i = 0; i < pokemonList.length; i++){
-            if (pokemonList[i].name == currentPokemon) {
-                document.querySelector('#pokedex-number').innerText = pokemonList[i].pokedexNumber;
-                document.querySelector('#pokemon-weight').innerText = pokemonList[i].weight + ' lbs';
-                document.querySelector('#pokemon-type').innerText = pokemonList[i].type;
+    //defines pokemonFound outside
+    var pokemonFound = false;
+    //This function writes down the pokemon's pokedex #, weight, and type
+    function pokemonStats(list){
+        for (let i = 0; i < list.length; i++){
+            if (list[i].name === currentPokemon) {
+                document.querySelector('#pokemon-name').innerText = properCase;
+                document.querySelector('#pokemon-Title').innerText = properCase;
+                document.querySelector('#pokedex-number').innerText = list[i].pokedexNumber;
+                document.querySelector('#pokemon-weight').innerText = list[i].weight + ' lbs';
+                document.querySelector('#pokemon-type').innerText = list[i].type;
+                pokemonFound = true;
+                createPokeImage(list[i].pokedexNumber,'pokemon-model');
+                break;
             }
         }
-
+        
+        for (let i = 0; i < list.length; i++){
+            if (list[i].name !== currentPokemon && pokemonFound === false){
+                document.querySelector('#pokemon-Title').innerText = 'ERROR';
+                document.querySelector('#pokemon-name').innerText = 'ERROR. POKEMON IS NOT IN DATABASE.';
+                document.querySelector('#pokedex-number').innerText = 'ERROR. POKEMON IS NOT IN DATABASE.';
+                document.querySelector('#pokemon-weight').innerText = 'ERROR. POKEMON IS NOT IN DATABASE.';
+                document.querySelector('#pokemon-type').innerText = 'ERROR. POKEMON IS NOT IN DATABASE.';
+            }
+        }
+    }
+        //checks to see if pokemon was already found in the first array or not.
         pokemonStats(pokemonList);
-        pokemonStats(pokemonList2);
-
-        //This creates a picture of said pokemon. That said, it is just a placeholder for now and has no way of using the array
-        // const pokemonModel = document.createElement('img');
-        // pokemonModel.src = 'https://placehold.co/600x400';
-        // var src = document.getElementById('pokemon-model');
-        // src.appendChild(pokemonModel);
+        if (pokemonFound === false){
+            pokemonStats(pokemonList2);
+        }
     }
 )
 
@@ -137,6 +148,7 @@ function pokedexEntry (list)
     document.querySelector('.pokemon-entry').innerHTML = pokemon;
 }
 
+//calls both functions
 pokedexEntry(pokemonList);
 pokedexEntry(pokemonList2);
 
