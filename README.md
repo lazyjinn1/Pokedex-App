80ba6d2dae396beafe47085fc9a08bc3c0a2ea6a
Jeriko Carrera <joshjbarba@gmail.com>
2023-09-21 15:54:09 -0700

    Updated for 1.6
    Added in clickable pokemon boxes and styled them. and coded the same utilization as the search bar.

--- README.md
+++ README.md
@@ -1,4 +1,54 @@
-Project description
-How to get the project running
-Project dependencies (e.g., JavaScript version, ESLint rules)
-Which API the project uses
+Objective
+To build a small web application with HTML, CSS, and JavaScript that loads
+data from an external API and enables the viewing of data points in detail.
+
+Context
+The best way to learn about programming is to complete a project using that programming language.
+This project aims to do just that—have you build a complete, fully functioning JavaScript web
+application. Since you haven’t yet learned how to build your own API, you’ll be relying on an external
+data source for this project.
+It’s best to have complete projects that you can showcase in your portfolio. For this reason, it’s
+important that you don’t limit your focus to JavaScript and JavaScript alone, but, instead, consider the
+app as a whole, including high-quality HTML and CSS. Your app should not only work, but be
+aesthetically pleasing and easy to use, as well. No one expects you to be a designer (nor to have the
+perfectly designed app a designer would have), but your final app shouldn’t look like a half-finished
+prototype.
+In the following sections, you’ll find more information regarding the requirements for your app.
+
+Features and Requirements
+
+User Goals
+Your users should be able to view a list of data and see more details for a given data item on demand.
+It’s up to you to come up with a type of data you want to display (see this list of public APIs for an
+example); however, if you don’t have any special preferences, you can write a small Pokédex app to
+display a list of Pokémon.
+
+Key Features
+● Load data from an external source (API)
+● View a list of items
+● On user action (e.g., by clicking on a list item), view details for that item
+
+Technical Requirements
+
+Required:
+● The app must load data from an external API; for instance, the Pokémon API.
+● The app must display a list of items loaded from that API after the page is loaded.
+● The app must enable the viewing of more details for a given list item (like a Pokémon) on
+demand, such as when clicking on a list item.
+● The app must have CSS styling.
+● The JavaScript code must be formatted according to ESLint rules.
+○ The JavaScript code may be formatted via Prettier.
+○ The JavaScript code may be manually formatted.
+● The app must use at least one additional complex UI pattern, such as a modal, for details or
+touch interactions.
+○ The app may allow searching for items (e.g., searching for Pokémon).
+● The app must not throw any errors when being used.
+● The app should be deployed to a publicly accessible platform like GitHub Pages (you can
+review how to do this in Intro to Frontend Development's Exercise 10: Code Quality, Testing, &
+Web Hosting).
+● The app must work in Chrome, Firefox, Safari, Edge, and Internet Explorer 11.
+
+Nice to Have:
+● The app should show loading indicators while loading data.
+● The app should handle errors (such as trying to load data while offline) and show user-friendly
+error messages.
--- js/scripts.js
+++ js/scripts.js
@@ -1,150 +1,164 @@
-let pokemonRepository = (function(){
-    let pokemonList = [
-    
-        {name: 'bulbasaur', pokedexNumber: '001', weight: 15, type: ['Grass','Poison'], evolvesInto: 'ivysaur'},
-        {name: 'charmander',  pokedexNumber: '004', weight: 19, type: 'Fire', evolvesInto: 'charmeleon'},
-        {name: 'squirtle', pokedexNumber: '007', weight: 19.8, type: 'Water', evolvesInto: 'wartortle'},
-        {name: 'pikachu', pokedexNumber: '025', weight: 13, type: 'Electric', evolvesInto: 'raichu'},
-        {name: 'eevee', pokedexNumber: '133', weight: 14, type: 'Normal', evolvesInto: ['jolteon','vaporeon','flareon','umbreon','espeon','leafeon','glaceon','sylveon']},
-        {name: 'nidoking', pokedexNumber: '034', weight: 137, type: ['Poison','Ground']},
-        {name: 'nidoqueen', pokedexNumber: '031', weight: 132, type: ['Poison','Ground']},
-        {name: 'chikorita', pokedexNumber: '152', weight: 14, type: 'Grass', evolvesInto: 'bayleaf'},
-        {name: 'cyndaquil',  pokedexNumber: '155', weight: 17, type: 'Fire', evolvesInto: 'quilava'},
-        {name: 'totodile', pokedexNumber: '158', weight: 21, type: 'Water', evolvesInto: 'crocanaw'},
-        {name: 'pichu', pokedexNumber: '172', weight: 4.4, type: 'Electric', evolvesInto: 'pikachu'},
-        {name: 'umbreon', pokedexNumber: '197', weight: 60, type: 'Dark'},
-        {name: 'espeon', pokedexNumber: '196', weight: 58, type: 'Psychic'},
-        {name: 'steelix', pokedexNumber: '208', weight: 885, type: ['Steel','Ground']}
-    ]
-    //returns full pokemonList
-    function getAll(){
-        return pokemonList;
-    }
-
-    //allows you to add pokemon into the array
-    function add(pokemon){
-            pokemonList.push(pokemon);
-    }
-
-    return {
-        getAll,
-        add
-    }
-})();
-
-//testing the add function (Question for Mentor/Tutor, "Why can't I add both of them in on the same add()?")
-pokemonRepository.add(
-    {name:'mewtwo', pokedexNumber:'150', weight: 269, type: 'Psychic'},
-);
-pokemonRepository.add(
-    {name:'mew', pokedexNumber:'151', weight: 8.8, type: 'Psychic'}
-);
-
-
-//This code checks for when the user clicks the button then executes the following code when the user does.
-document.querySelector('button').addEventListener('click',()=>{
-
-    //function for removing the image of the pokemon so they don't infinitely stack down.
-    function removePokeImage(containerDiv){
-        var img = document.getElementById(containerDiv);
-        img.innerHTML = ''; 
-    }
-
-    //function for adding in the pictures for pokemon
-    function createPokeImage(pokeID, containerDiv){
-        var pokeImage = document.createElement('img');
-        pokeImage.srcset =`https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/${pokeID}.png`
-        var div = document.getElementById(containerDiv);
-        div.appendChild(pokeImage);
-        console.log(div);
-    }
-
-    //function that triggers a pokemon's voice when summoned
-    function playPokemonCry(pokeID){
-        let pureNumber = Number(pokeID);
-        var audio = new Audio(`assets/cries/${pureNumber}.wav`);
-        audio.play();
-    }
-
-    //This is the pokemon that is typed in the textbox
-    let pokemonInputted = document.querySelector('#Pokemon').value;
-    //console.log(pokemonInputted);
-
-    //This is here to make it case insensitive
-    let currentPokemon = pokemonInputted.toLowerCase();
-    //console.log(currentPokemon);
-
-    //capitalizes the pokemon's name in the title and pokedex entry
-    let properCase = currentPokemon.charAt(0).toUpperCase() + currentPokemon.substring(1);
-    //console.log(properCase);
-
-    var pokemonFound = false;
-
-    //This function writes down the pokemon's pokedex #, weight, and type
-    function pokemonStats(list){
-        const selected = list.find(item => item.name==currentPokemon)
-        if(selected){
-            if (selected.name === currentPokemon) 
-            {
-                console.log(selected)
-                document.querySelector('#pokemon-name').innerText = properCase;
-                document.querySelector('#pokemon-Title').innerText = properCase;
-                document.querySelector('#pokedex-number').innerText = '#' + selected.pokedexNumber;
-                document.querySelector('#pokemon-weight').innerText = selected.weight + ' lbs';
-                document.querySelector('#pokemon-type').innerText = selected.type;
-                pokemonFound = true;
-                removePokeImage('pokemon-model');
-                createPokeImage(selected.pokedexNumber,'pokemon-model');
-                playPokemonCry(selected.pokedexNumber);
-                console.log(selected.pokedexNumber)
-            }
-        }
-            else{
-                console.log(selected)
-                document.querySelector('#pokemon-Title').innerText = properCase;
-                document.querySelector('#pokemon-name').innerText = 'ERROR. '+ currentPokemon.toUpperCase() + ' IS NOT IN DATABASE.';
-                document.querySelector('#pokedex-number').innerText = 'ERROR. '+ currentPokemon.toUpperCase() + ' IS NOT IN DATABASE.';
-                document.querySelector('#pokemon-weight').innerText = 'ERROR. '+ currentPokemon.toUpperCase() + ' IS NOT IN DATABASE.';
-                document.querySelector('#pokemon-type').innerText = 'ERROR. '+ currentPokemon.toUpperCase() + ' IS NOT IN DATABASE.';
-                removePokeImage('pokemon-model');
-            }
-    };
-    //executes Function
-    pokemonStats(pokemonRepository.getAll());;
-});
 //variables to measure lightness and heaviness
 const lightPokemon = 20;
 const heavyPokemon = 80;
 const insanelyHeavyPokemon = 200;
 
-//empty variable used as a placeholder for writing everything down onto a list form on the HTML page
-var pokemon = '';
+//empty variables used as placeholders for writing everything down onto a list form on the HTML page
+var pokemonNameEntry = '';
+var pokemonNumberEntry = '';
+var pokemonWeightEntry = '';
+var pokemonTypeEntry = '';
+
+//default false value if pokemon is not found in array
+var pokemonFound = false;
+
+let pokemonRepository = (function () {
+    let pokemonList = [
+
+        { name: 'bulbasaur', pokedexNumber: '001', weight: 15, type: ['Grass', 'Poison'], evolvesInto: 'ivysaur' },
+        { name: 'charmander', pokedexNumber: '004', weight: 19, type: 'Fire', evolvesInto: 'charmeleon' },
+        { name: 'squirtle', pokedexNumber: '007', weight: 19.8, type: 'Water', evolvesInto: 'wartortle' },
+        { name: 'pikachu', pokedexNumber: '025', weight: 13, type: 'Electric', evolvesInto: 'raichu' },
+        { name: 'eevee', pokedexNumber: '133', weight: 14, type: 'Normal', evolvesInto: ['jolteon', 'vaporeon', 'flareon', 'umbreon', 'espeon', 'leafeon', 'glaceon', 'sylveon'] },
+        { name: 'nidoking', pokedexNumber: '034', weight: 137, type: ['Poison', 'Ground'] },
+        { name: 'nidoqueen', pokedexNumber: '031', weight: 132, type: ['Poison', 'Ground'] },
+        { name: 'chikorita', pokedexNumber: '152', weight: 14, type: 'Grass', evolvesInto: 'bayleaf' },
+        { name: 'cyndaquil', pokedexNumber: '155', weight: 17, type: 'Fire', evolvesInto: 'quilava' },
+        { name: 'totodile', pokedexNumber: '158', weight: 21, type: 'Water', evolvesInto: 'crocanaw' },
+        { name: 'pichu', pokedexNumber: '172', weight: 4.4, type: 'Electric', evolvesInto: 'pikachu' },
+        { name: 'umbreon', pokedexNumber: '197', weight: 60, type: 'Dark' },
+        { name: 'espeon', pokedexNumber: '196', weight: 58, type: 'Psychic' },
+        { name: 'steelix', pokedexNumber: '208', weight: 885, type: ['Steel', 'Ground'] }
+    ]
+    //returns full pokemonList
+    function getAll() {
+        return pokemonList;
+    }
+
+    //allows you to add pokemon into the array
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    //writes down the pokemon in an order as buttons
+    function addPokeButton(pkmn) {
+        pokemonNameEntry = pkmn.name.charAt(0).toUpperCase() + pkmn.name.substring(1) + '<br>';
+        pokemonNumberEntry = 'Pokedex #' + pkmn.pokedexNumber + '<br>';
+        pokemonWeightEntry = pkmn.weight + ' lbs' + '<br>';
+        pokemonTypeEntry = pkmn.type + '<br>';
+
+        let container = document.querySelector('.pokemon-entry');
+        let button = document.createElement('button');
+        document.querySelector('button');
+        button.addEventListener('click',function(){
+            showDetails(pkmn);
+        });
+        button.innerHTML = pokemonNameEntry + pokemonNumberEntry + pokemonWeightEntry + pokemonTypeEntry;
+        button.classList.add('dex-entry');
+        container.appendChild(button);
+        
+    }
+
+    function showDetails(pkmn){
+        document.querySelector('#pokemon-name').innerHTML = pkmn.name.charAt(0).toUpperCase() + pkmn.name.substring(1) + '<br>';
+        document.querySelector('#pokemon-Title').innerHTML = pkmn.name.charAt(0).toUpperCase() + pkmn.name.substring(1) + '<br>';
+        document.querySelector('#pokedex-number').innerHTML = '#' + pkmn.pokedexNumber;
+        document.querySelector('#pokemon-weight').innerHTML = pkmn.weight + ' lbs';
+        document.querySelector('#pokemon-type').innerHTML = pkmn.type;
+        document.body.scrollTop = document.documentElement.scrollTop = 0;
+        removePokeImage('pokemon-model');
+        createPokeImage(pkmn.pokedexNumber, 'pokemon-model');
+        playPokemonCry(pkmn.pokedexNumber);
+        console.log(pkmn);
+    }
+
+    return {
+        getAll,
+        add,
+        addPokeButton
+    }
+})();
+
+//testing the add function (Question for Mentor/Tutor, "Why can't I add both of them in on the same add()?")
+pokemonRepository.add(
+    { name: 'mewtwo', pokedexNumber: '150', weight: 269, type: 'Psychic' },
+    //{name:'mew', pokedexNumber:'151', weight: 8.8, type: 'Psychic'}
+);
+pokemonRepository.add(
+    { name: 'mew', pokedexNumber: '151', weight: 8.8, type: 'Psychic' }
+);
+
+//function for removing the image of the pokemon so they don't infinitely stack down.
+function removePokeImage(containerDiv) {
+    var img = document.getElementById(containerDiv);
+    img.innerHTML = '';
+}
+
+//function for adding in the pictures for pokemon
+function createPokeImage(pokeID, containerDiv) {
+    var pokeImage = document.createElement('img');
+    pokeImage.srcset = `https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/${pokeID}.png`
+    var div = document.getElementById(containerDiv);
+    div.appendChild(pokeImage);
+    pokeImage.setAttribute('id', 'pokeImage');
+    console.log(div);
+}
+
+//function that triggers a pokemon's voice when summoned
+function playPokemonCry(pokeID) {
+    let pureNumber = Number(pokeID);
+    var audio = new Audio(`assets/cries/${pureNumber}.wav`);
+    audio.play();
+}
+
+
+//This code checks for when the user clicks the button then executes the following code when the user does.
+document.querySelector('button').addEventListener('click', () => {
+    //This is the pokemon that is typed in the textbox
+    let pokemonInputted = document.querySelector('#Pokemon').value;
+
+    //This is here to make it case insensitive
+    let currentPokemon = pokemonInputted.toLowerCase();
+
+    //capitalizes the pokemon's name in the title and pokedex entry
+    let properCase = currentPokemon.charAt(0).toUpperCase() + currentPokemon.substring(1);
+
+    //This function writes down the pokemon's pokedex #, weight, and type
+    function pokemonStats(list) {
+        const selected = list.find(item => item.name == currentPokemon)
+        if (selected) {
+            if (selected.name === currentPokemon) {
+                console.log(selected)
+                document.querySelector('#pokemon-name').innerHTML = properCase;
+                document.querySelector('#pokemon-Title').innerHTML = properCase;
+                document.querySelector('#pokedex-number').innerHTML = '#' + selected.pokedexNumber;
+                document.querySelector('#pokemon-weight').innerHTML = selected.weight + ' lbs';
+                document.querySelector('#pokemon-type').innerHTML = selected.type;
+                pokemonFound = true;
+                removePokeImage('pokemon-model');
+                createPokeImage(selected.pokedexNumber, 'pokemon-model');
+                playPokemonCry(selected.pokedexNumber);
+                console.log(selected.pokedexNumber)
+            }
+        }
+        else {
+            console.log(selected)
+            document.querySelector('#pokemon-Title').innerText = properCase;
+            document.querySelector('#pokemon-name').innerText = 'ERROR. ' + currentPokemon.toUpperCase() + ' IS NOT IN DATABASE.';
+            document.querySelector('#pokedex-number').innerText = 'ERROR. ' + currentPokemon.toUpperCase() + ' IS NOT IN DATABASE.';
+            document.querySelector('#pokemon-weight').innerText = 'ERROR. ' + currentPokemon.toUpperCase() + ' IS NOT IN DATABASE.';
+            document.querySelector('#pokemon-type').innerText = 'ERROR. ' + currentPokemon.toUpperCase() + ' IS NOT IN DATABASE.';
+            removePokeImage('pokemon-model');
+        }
+    };
+    //executes Function
+    pokemonStats(pokemonRepository.getAll());;
+});
+
 
 //forEach loop which checks for weight and gives values for each pokemons' properties
-function pokedexEntry (list)
-{
-    list.forEach(function(pkmn){
-        if(pkmn.weight >= insanelyHeavyPokemon){
-            weightChecker = '(Good luck carrying this pokemon!)';
-        }
-        else if(pkmn.weight > heavyPokemon){
-            weightChecker = '(This pokemon is heavy!)';
-        }
-        else if (pkmn.weight < heavyPokemon && pkmn.weight > lightPokemon){
-            weightChecker = '(This pokemon is average)';
-        }
-        else{
-            weightChecker = '(This pokemon is light!)';
-        }
+pokemonRepository.getAll().forEach(function(pkmn) {
+    pokemonRepository.addPokeButton(pkmn)
+});
 
-        pokemon += pkmn.name.charAt(0).toUpperCase()+ pkmn.name.substring(1) + weightChecker + '<br>' ;
-        pokemon += 'Pokedex Number: #' + pkmn.pokedexNumber + '<br>';
-        pokemon += pkmn.weight + ' lbs' + '<br>';
-        pokemon += pkmn.type + '<br>' + '<br>' + '<br>';
-        
-        //add a class name 
-        document.querySelector('.pokemon-entry').innerHTML = pokemon;
-    }); 
-}
-console.log(pokemonRepository.getAll());
-pokedexEntry(pokemonRepository.getAll());
+    
